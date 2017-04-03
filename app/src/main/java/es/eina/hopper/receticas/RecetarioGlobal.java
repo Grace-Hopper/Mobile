97ef90317e4f2a731aaf7b51e5461e1c70cf4b00
Davy <davidmarquezcalavia@gmail.com>
2017-04-03 19:29:20 +0200

    acerca de

--- app/src/main/java/es/eina/hopper/receticas/RecetarioGlobal.java
+++ app/src/main/java/es/eina/hopper/receticas/RecetarioGlobal.java
@@ -111,5 +111,14 @@ public class RecetarioGlobal extends AppCompatActivity
         return true;
     }
 
+<<<<<<< Updated upstream
+=======
+    /*public ArrayList<Receta> lista_recetas=new ArrayList<Receta>();
+    ArrayAdapter<Receta> adaptador =(new ArrayAdapter<Receta>(this, R.layout.lista_layout,
+                                R.id.nombre_receta, lista_recetas));*/
+
+
+
+>>>>>>> Stashed changes
 
 }
