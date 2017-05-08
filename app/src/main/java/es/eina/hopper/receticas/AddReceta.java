35088e73d214e29296a6bb29efee1369369d815c
ferdelmo <ferdelmo1996@gmail.com>
2017-05-08 22:20:34 +0200

    CHANGES en añadir

--- app/src/main/java/es/eina/hopper/receticas/AddReceta.java
+++ app/src/main/java/es/eina/hopper/receticas/AddReceta.java
@@ -12,6 +12,9 @@ import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -43,6 +46,7 @@ import java.util.Objects;
 import es.eina.hopper.models.Recipe;
 import es.eina.hopper.models.User;
 
+import static android.R.attr.fragment;
 import static android.support.v4.view.PagerAdapter.POSITION_NONE;
 
 public class AddReceta extends AppCompatActivity {
@@ -100,29 +104,67 @@ public class AddReceta extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(mViewPager,lp,rec);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int current = mViewPager.getCurrentItem();
-                lp.add(current, new PasosDetalle("EL TROLEITO ES REAL", numPasos));
+                PasosDetalle a = new PasosDetalle("EL TROLEITO ES REAL", numPasos);
+                lp.add(current, a);
+                PlaceholderFragment b = new PlaceholderFragment();
+                b.setArguments(a,numPasos);
+                ViewPagerAdapter adapter = (ViewPagerAdapter)mViewPager.getAdapter();
                 numPasos++;
-                setupViewPager(mViewPager,lp,rec);
-                mViewPager.setCurrentItem(current+1);
+                adapter.addFrag(b, "PASO " + (numPasos));
+                mViewPager.setAdapter(adapter);
+                mViewPager.setCurrentItem(current+1,true);
             }
         });
 
-        FloatingActionButton borrar = (FloatingActionButton) findViewById(R.id.borrar);
+        final FloatingActionButton borrar = (FloatingActionButton) findViewById(R.id.borrar);
+        borrar.setVisibility(View.GONE);
         borrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int current = mViewPager.getCurrentItem();
-                mViewPager.removeViewAt(current+1);
-                mViewPager.setCurrentItem(current-1);
+                lp.remove(current-1);
+                ViewPagerAdapter adapter = (ViewPagerAdapter)mViewPager.getAdapter();
+                numPasos--;
+                System.out.println("FRAGMENTS: " + numPasos + " ____ REALES: " + getSupportFragmentManager().getFragments().size());
+                getSupportFragmentManager().beginTransaction().remove(adapter.getItem(current)).commitNow();
+                System.out.println("FRAGMENTS: " + numPasos + " ____ REALES: " + getSupportFragmentManager().getFragments().size());
+                adapter.remove(current);
+                adapter.actualizarNom();
+                mViewPager.setAdapter(adapter);
+                mViewPager.setCurrentItem(current-1,true);
+            }
+        });
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                int numTab = tab.getPosition();
+                mViewPager.setCurrentItem(numTab);
+                if(numTab==numPasos && numTab>=1){
+                    borrar.setVisibility(View.VISIBLE);
+                }
+                else{
+                    borrar.setVisibility(View.GONE);
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
@@ -149,12 +191,17 @@ public class AddReceta extends AppCompatActivity {
             b.setArguments(lp.get(i),i);
             adapter.addFrag(b, "PASO " + (i+1));
         }
+        viewPager.removeAllViews();
+        adapter.compro();
         viewPager.setAdapter(adapter);
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
+    public class ViewPagerAdapter extends FragmentStatePagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
         private final List<String> mFragmentTitleList = new ArrayList<>();
+        private final List<View> mListView = new ArrayList<>();
+        int baseId=0;
+
 
         public ViewPagerAdapter(FragmentManager manager) {
             super(manager);
@@ -166,6 +213,26 @@ public class AddReceta extends AppCompatActivity {
         }
 
         @Override
+        public int getItemPosition(Object object) {
+            System.out.println("BORRRAAAAR");
+            for(int i=0;i<mFragmentTitleList.size();i++){
+                System.out.println(mFragmentTitleList.get(i));
+            }
+            if (mFragmentList.contains((Fragment) object)) {
+                return mFragmentList.indexOf((Fragment) object);
+            } else {
+                System.out.println("JODER DEBERIA DE IR OSTIA PUTA");
+                return POSITION_NONE;
+            }
+        }
+
+        public void actualizarNom(){
+            for(int i=1;i<mFragmentTitleList.size();i++){
+                mFragmentTitleList.set(i,"PASO " + i);
+            }
+        }
+
+        @Override
         public int getCount() {
             return mFragmentList.size();
         }
@@ -173,6 +240,26 @@ public class AddReceta extends AppCompatActivity {
         public void addFrag(Fragment fragment, String title) {
             mFragmentList.add(fragment);
             mFragmentTitleList.add(title);
+            mListView.add(fragment.getView());
+            notifyDataSetChanged();
+        }
+        public void remove(int position){
+            mFragmentList.remove(position);
+            mFragmentTitleList.remove(position);
+            mListView.remove(position);
+            notifyDataSetChanged();
+        }
+        public void removeAll(){
+            while(mFragmentList.size()!=0){
+                mFragmentList.remove(0);
+                mFragmentTitleList.remove(0);
+                notifyDataSetChanged();
+            }
+        }
+        public void compro(){
+            for(int i=0;i<mFragmentList.size();i++){
+                System.out.println("ESTA MIERDA FUNCA " + mFragmentList.get(i));
+            }
         }
 
         @Override
@@ -216,10 +303,15 @@ public class AddReceta extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_add_receta, container, false);
-            //TextView aux = (TextView) rootView.findViewById(R.id.section_label);
+            //TextView aux = (TextView) rootView.findViewById(R.id.section_label)
 
             //System.out.println("VOY A MOSTRAR " + pd.tiempo);
             //aux.setText(pd.contenido + "\n TIEMPO = " + pd.tiempo + " - YO:" + yo);
+            //EditText tiempo = (EditText) rootView.findViewById(R.id.tiempo);
+           // tiempo.setText(pd.tiempo);
+            EditText tiempo = (EditText) rootView.findViewById(R.id.tiempoReceta);
+            EditText desc = (EditText) rootView.findViewById(R.id.descripcionPasos);
+            //desc.setText("COJON DE PUTAS " + pd.tiempo);
             return rootView;
         }
     }
